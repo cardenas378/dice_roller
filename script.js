6be7510b7fb3418eba183d74ba7cd50039fe9e4e
Juan Cardenas <cardenas378@gmail.com>
2019-12-07 17:12:43 -0800

    added images of dice and the ability to randomize images

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ var number = document.getElementById("number")
 
 function generateNumber(){
   var random = Math.floor(Math.random() * 6) + 1;
+  var diceImg = "img/dice"+ random + ".png"
+  document.querySelector('.img1').setAttribute('src', diceImg);
+
   return random;
 }
 
 button.addEventListener("click", function(){
     var result = generateNumber();
+    document.getElementById('result').innerHTML = '<img src="dice' + RandomNumber1 + '.png">';
     number.innerHTML = result;
 })
